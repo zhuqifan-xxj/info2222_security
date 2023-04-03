08cf7a46648f0e94af638a1c908bdd78175a8fb3
firstname lastname <youremail@email.com>
2023-04-03 15:29:43 +0800

    test

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def news():
+    the_news = {
+        'XXX1': '1',
+        'XXX2': '2',
+        'XXX3': '3',
+        'XXX4': '4',
+    }
+    context = {
+        'title': '新闻',
+        'the_news': the_news,
+    }
+    return render_template('index.html', context=context)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=80)
